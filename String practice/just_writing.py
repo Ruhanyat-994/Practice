650c0096ee8a841cb6762ba7fd43f19098b3b37e
Mian Al Ruhanyat <110297704+Ruhanyat-994@users.noreply.github.com>
2022-12-04 03:55:54 +0600

    Add files via upload

---
+++ String practice/just_writing.py
@@ -0,0 +1,15 @@
+import math
+print("Hello world")
+#print(math.floor(4.2345))
+#print(math.tan(90))
+#a =1 
+#b =  "6"
+#ab = print(a+int(b))
+
+#str1 = "Hello this is my first string"
+#print(str1.find("first"))
+
+
+list = [1,3,4]
+list[2]="Hello"
+print("This is a list ", list)
