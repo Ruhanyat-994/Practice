f8a4f0580dc44640ddaa85e9a0797af905d363ab
Mian Al Ruhanyat <110297704+Ruhanyat-994@users.noreply.github.com>
2022-12-13 08:18:21 +0600

    Add files via upload

---
+++ Condition practice/12_wheather_app.py
@@ -0,0 +1,13 @@
+'''The wheather app'''
+
+wheather = input("Wheather check :")
+if wheather == "Rain":
+  
+  print("☔")
+elif wheather == "Thunderstorm":
+  print("⛈️")
+elif wheather == "Cloudy":
+  print("☁")
+
+else:
+  print("😎")
